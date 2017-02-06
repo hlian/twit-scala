c6ded11867888273539745778ef30e63391024ea
matt handler <matt.handler@gmail.com>
2017-02-06 14:50:13 -0500

    add additional config pieces

--- app/Config.scala
+++ app/Config.scala
@@ -1,7 +1,17 @@
 package com.originate.config
 
+import com.originate.global.Environment
+
 case class Config(
-    auth0: AuthZeroConfig
+    applicationName: String,
+    environment: Environment,
+    auth0: AuthZeroConfig,
+    datadog: Datadog
+)
+
+case class Datadog(
+    agentHost: String,
+    agentPort: Int
 )
 
 case class AuthZeroConfig(
