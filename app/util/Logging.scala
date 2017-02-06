90bffbb63d628eaca25bf78e8b9cfee82ea04f63
matt handler <matt.handler@gmail.com>
2017-02-06 16:38:21 -0500

    add docstring for logging trait

--- app/util/Logging.scala
+++ app/util/Logging.scala
@@ -2,6 +2,9 @@ package com.originate.util
 
 import play.api.Logger
 
+/**
+ * Simple mixin trait to provide a logger to any class
+ */
 trait Logging {
   protected val logger: Logger = Logger(this.getClass())
 
